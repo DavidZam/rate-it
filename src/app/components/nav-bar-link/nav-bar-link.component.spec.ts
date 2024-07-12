bd357b803f162b384c2e6d6bc489b91fd82c7e7c
David Zamora <davidzamorarey@gmail.com>
2024-07-12 10:49:17 +0200

    fix: :truck: nav-bar-item component rename to nav-bar-link

--- src/app/components/nav-bar-link/nav-bar-link.component.spec.ts
+++ src/app/components/nav-bar-link/nav-bar-link.component.spec.ts
@@ -1,17 +1,17 @@
 import { RouterLink } from '@angular/router';
 import { render, screen } from '@testing-library/angular';
 import '@testing-library/jest-dom';
-import { NavBarItemComponent } from './nav-bar-item.component';
+import { NavBarLinkComponent } from './nav-bar-link.component';
 
-describe('NavBarItem', () => {
+describe('NavBarLink', () => {
   it('should render', async () => {
-    await render(NavBarItemComponent, {
+    await render(NavBarLinkComponent, {
       imports: [RouterLink],
     });
   });
 
   it('should display the nav bar item link with the first letter on upper case on render', async () => {
-    await render(NavBarItemComponent, {
+    await render(NavBarLinkComponent, {
       imports: [RouterLink],
       componentProperties: {
         itemLink: 'link1',
--- src/app/components/nav-bar-link/nav-bar-link.component.ts
+++ src/app/components/nav-bar-link/nav-bar-link.component.ts
@@ -2,12 +2,12 @@ import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-nav-bar-item',
+  selector: 'app-nav-bar-link',
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './nav-bar-item.component.html',
-  styleUrl: './nav-bar-item.component.css',
+  templateUrl: './nav-bar-link.component.html',
+  styleUrl: './nav-bar-link.component.css',
 })
-export class NavBarItemComponent {
+export class NavBarLinkComponent {
   @Input({ required: true }) itemLink = '';
 }
